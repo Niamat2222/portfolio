fdc92417d745be63a03e658f811301cef9151e5b
Niamat <niamatullah.akbari@opp.eduvantaa.fi>
2022-01-20 14:52:49 +0200

    pesonalportfolio

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,29 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import Main from './components/sites/main';
+import Cv from './components/sites/cv';
+import Projects from './components/sites/projects';
 
 const App: React.FC = () => {
   return (
       // Important! add your repositoryname to basename
-      <Router basename='/YourRepoNameHere'>
+      <Router basename='/portfolio'>
       
         {/* The navigation bar and other components you want to display on all pages come here */}
         <div className={styles.navbar}>
           <Link to='/'>Front page</Link>
-          <Link to='example'>Look my example component</Link>
+          <Link to='/projects'>projects</Link>
+          <Link to='/cv'>Cv</Link>
         </div>
   
         <Switch>
           {/* Changing content comes here */}
-          <Route exact path='/' component={MyFirstComponent} />
-          <Route exact path='/example' component={SomeExampleComponent}/>
+          {/* <Route exact path='/' component={MyFirstComponent} /> */}
+          {/* <Route exact path='/example' component={SomeExampleComponent}/> */}
+          <Route exact path={"/"} component={Main} />
+          <Route exact path={"/cv"} component={Cv} />
+          <Route exact path={"/projects"} component={Projects} />
         </Switch>
   
         {/* The footer and other components you want to display on all pages come here */}
