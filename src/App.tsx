04f8aed855df0c188fa37b82d878bb7b50e53787
Niamat <niamatullah.akbari@opp.eduvantaa.fi>
2022-02-01 15:07:59 +0200

    changed website profile

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import Main from './components/sites/main';
+import about from './components/sites/about';
 import Cv from './components/sites/cv';
 import Projects from './components/sites/projects';
-import img2 from "./media/img2.png"
-import img1 from "./media/img1.png"
+{/*import img3 from "./media/img3.png"*/}
+import website1 from "./media/website1.png"
 const App: React.FC = () => {
   return (
       // Important! add your repositoryname to basename
@@ -22,23 +22,23 @@ const App: React.FC = () => {
          <nav>
            <ul>
                <li><a href=".">HOME</a></li>
-               <li><a href=".">ABOUT</a></li>
+               <li><a href="/about">ABOUT</a></li>
                <li><a href="/projects">PORTFOLIO</a></li>
                <li><a href="/cv">CV</a></li>
            </ul>
          </nav>
-         <div className={styles.detail}>
+         {/*<div className={styles.detail}>
          <h1>Welcome to <span>Dev Discover</span></h1>
          <p>This is my Portfolio Website</p>
          <p>Details and work experince web development.</p>
-         </div>
+         </div>*/}
          <div className= "images">
-         <img className={styles.img2} src={img2} alt='back' />
-         <img className={styles.img1} src={img1} alt='back' />
-         </div>
+         {/*<img className={styles.img3} src={img3} alt='back' />*/}
+         <img className={styles.website1} src={website1} alt='back' />
+  </div>
         </div>
         <Switch>
-          <Route exact path={"/"} component={Main} />
+          <Route exact path={"/"} component={about} />
           <Route exact path={"/cv"} component={Cv} />
           <Route exact path={"/projects"} component={Projects} />
         </Switch>
---
+++ src/components/sites/about.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+type MainProps = {
+
+}
+
+const Main: React.FC<MainProps> = () => {
+  return (
+    <div className="main">
+       <nav>
+    <ul>
+      </ul>
+    </nav>
+
+    </div>
+  )
+}
+
+export default Main
