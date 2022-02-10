d4ea75ef4b6507c14866da41cf0199c794ed6e35
Niamat <niamatullah.akbari@opp.eduvantaa.fi>
2022-02-10 14:44:36 +0200

    my img

--- src/components/sites/cv.tsx
+++ src/components/sites/cv.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './cv.module.scss'
 import Banner from './bgpic/pic'
+import pics from '../../media/me.png'
 type CvProps = {
 
 }
@@ -11,8 +12,8 @@ const Cv: React.FC<CvProps> = () => {
        <div className={styles.container}>
        <Banner text='CV' className={styles.pic}/>
       </div>
-      <div className='images'>
-        <img src='me.png' alt='me.png'></img> 
+      <div className={styles.niamatPicFrame} >
+      <img className={styles.niamatPic} src={pics}  alt="Italian mafia"></img>
        </div>
     </div>
   )
