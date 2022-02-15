c4884914670f79caa4bfb7875dcf523dc4e3869f
Niamat <niamatullah.akbari@opp.eduvantaa.fi>
2022-02-15 15:40:17 +0200

    addprojects

--- src/components/sites/projects.tsx
+++ src/components/sites/projects.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import styles from './project.module.scss'
 import Banner from './bgpic/pic'
 import pics from '../../media/cartoon@.png'
-
+import pics1 from '../../media/IC.png'
+import pics2 from '../../media/CE.png'
+import pics3 from '../../media/IOT.png'
 type ProjectsProps = {
 
 }
@@ -14,7 +16,22 @@ const Projects: React.FC<ProjectsProps> = () => {
      <p className={styles.title}> Projects are here </p>
      <div className={styles.Projects}>
        <ul>
-         <a href='https://play.unity.com/u/Niamatcom/'><img className={styles.GmProjects} src={pics}  alt="Dragon"></img></a>
+         <a href='https://play.unity.com/u/Niamatcom/'>
+          <img className={styles.GmProjects} src={pics}  alt="Dragon"/>
+          <p>Click On Play.Unity</p>
+         </a>
+         <a href='https://www.credly.com/badges/77061c91-e3ca-4ca0-bb66-53fb50095001/public_url/'>
+          <img className={styles.GmProjects} src={pics1}  alt="Dragon"/>
+          <p>Click On My Badge</p>
+         </a>
+         <a href='https://www.credly.com/badges/4943d606-6063-4079-bcf5-e4e3470da663/public_url/'>
+          <img className={styles.GmProjects} src={pics2}  alt="Dragon"/>
+          <p>Click On My Badge</p>
+         </a>
+         <a href='https://www.credly.com/badges/78216aea-da3e-4cfd-9834-1040c7367d61/public_url/'>
+          <img className={styles.GmProjects} src={pics3}  alt="Dragon"/>
+          <p>Click On My Badge</p>
+         </a>
        </ul>
      </div>
     </div>
