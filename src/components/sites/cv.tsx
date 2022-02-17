5d9c0df9160d66e72f590f23280a2f68b8be46fa
Niamat2222 <niamat.akbari@outlook.com>
2022-02-17 14:18:29 +0200

    chanegedtextfonts

--- src/components/sites/cv.tsx
+++ src/components/sites/cv.tsx
@@ -16,25 +16,25 @@ const Cv: React.FC<CvProps> = () => {
        <img className={styles.myimg} src={pics}  alt="Niamat"/>
        </div>
        <div className={styles.activities}>
-         <h1>PROGRAMMING LANGUAGES&#128037;
-           <p>Java&#128330;, TypeScript&#128330;, JavaScript&#128330;, C#&#128330;, Unity&#128330;, React&#128330;, SQL&#128330;, Node Sql&#128330;, HTML&#128330;, CSS&#128330;, ReactJs</p>
+         <h1>PROGRAMMING LANGUAGES
+           <p>Java, TypeScript, JavaScript, C#, Unity, React, SQL, Node Sql, HTML, CSS, ReactJs</p>
          </h1>
        </div>
        <div className={styles.activities}>
-         <h1>WORK EXPERIENCE&#128037;
-           <p>3/4/2019 - 3/15/2019&#128330;	Trainee Tokmanni of Myyrmäki Cleaning, Shelving, Customer Service</p>
-           <p>3/15/2020 – 3/27/2020&#128330; Trainee Martinlaakson’s  S_Market Shelving, Customer Service</p>
-           <p>1/10/2020 - now&#128330; Job Foodora/Wolt food delivery</p>
+         <h1>WORK EXPERIENCE
+           <p>3/4/2019 - 3/15/2019	Trainee Tokmanni of Myyrmäki Cleaning, Shelving, Customer Service</p>
+           <p>3/15/2020 – 3/27/2020 Trainee Martinlaakson’s  S_Market Shelving, Customer Service</p>
+           <p>1/10/2020 - now Job Foodora/Wolt food delivery</p>
          </h1>
        </div>
        <div className={styles.activities}>
-         <h1>TRAINING&#128037;
-           <p>8 / 2017&#128330; Primary school for adults Vantaa Adult Education Center, Vantaa
+         <h1>TRAINING
+           <p>8 / 2017 Primary school for adults Vantaa Adult Education Center, Vantaa
               I study Finnish and the curriculum of a Finnish comprehensive school</p>
          </h1>
        </div>
        <div className={styles.activities}>
-         <h1>HOBBIES&#128037;
+         <h1>HOBBIES
            <p>Gym, swimming, </p>
          </h1>
        </div>
