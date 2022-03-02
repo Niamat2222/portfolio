0331d659cde6cdfc0b296a41c5c47e9c079dfc70
Niamat <niamatullah.akbari@opp.eduvantaa.fi>
2022-03-02 11:33:38 +0200

    teinCV

--- src/components/sites/cv.tsx
+++ src/components/sites/cv.tsx
@@ -19,11 +19,7 @@ const Cv: React.FC<CvProps> = () => {
          <h1>ABOUT ME&#128330;
            <p>I am studying Information and Communication Technology in Vocational Qualification.
               The planned graduation from the school is 6/3/2023. In the afternoon I work at Wolt as a food courier.</p>
-
-           <p>I studied at the Vantaa Adult Primary School, during the primary school I did an internship twice and everyone lasted for 2 weeks, 
-              I have also worked at Eezy and Varapu a few times in grocery stores.</p>
-           <p>In working life, I have done teamwork and sometimes I was alone.</p>
-           <p>I am hardworking, brisk and friendly I like to work in a team and alone.</p>
+           <p>I studied at the Vantaa Adult Primary School, At the adult primary school, I studied Finnish and Finnish primary school.</p>
          </h1>
          <h1>PROGRAMMING LANGUAGES&#128330;
            <p>Java, TypeScript, JavaScript, C#, Unity, React, SQL, Node Sql, HTML, CSS, ReactJs</p>
@@ -33,13 +29,9 @@ const Cv: React.FC<CvProps> = () => {
          <h1>WORK EXPERIENCE&#128330;
            <p>3/4/2019 - 3/15/2019&#128330;	Trainee Tokmanni of Myyrmäki Cleaning, Shelving, Customer Service</p>
            <p>3/15/2020 – 3/27/2020&#128330; Trainee Martinlaakson’s  S_Market Shelving, Customer Service</p>
+           <p>I have worked at Eezy and Varapu a few times in grocery stores.</p>
+           <p>In working life, I have done teamwork and sometimes I was alone.</p>
            <p>1/10/2020 - now&#128330; Job Foodora/Wolt food delivery</p>
-         </h1>
-       </div>
-       <div className={styles.activities}>
-         <h1>TRAINING&#128330;
-           <p>8 / 2017&#128330; Primary school for adults Vantaa Adult Education Center, Vantaa
-              I study Finnish and the curriculum of a Finnish comprehensive school</p>
          </h1>
        </div>
        <div className={styles.activities}>
