68ed781b5887f2a0597872c833065eb773e8cb42
Niamat <niamatullah.akbari@opp.eduvantaa.fi>
2022-03-17 20:19:34 +0200

    cv

--- src/components/sites/cv.tsx
+++ src/components/sites/cv.tsx
@@ -20,7 +20,7 @@ const Cv: React.FC<CvProps> = () => {
          <h1>ABOUT ME&#128330;
            <p>I am studying Information and Communication Technology in Vocational Qualification.
               The planned graduation from the school is 6/3/2023. In the afternoon I work at Wolt as a food courier.</p>
-           <p>I studied at the Vantaa Adult Primary School, At the adult primary school, I studied finnish langueg and Finnish primary school.</p>
+           <p>I studied at the Vantaa Adult Primary School, At the adult primary school, I studied finnish language and Finnish primary school.</p>
          </h1>
          <h1>PROGRAMMING LANGUAGES&#128330;
            <p>Java, TypeScript, JavaScript, C#, Unity, React, SQL, Node Sql, HTML, CSS, ReactJs</p>
@@ -30,7 +30,7 @@ const Cv: React.FC<CvProps> = () => {
          <h1>WORK EXPERIENCE&#128330;
            <p>3/4/2019 - 3/15/2019&#128330;	Trainee Tokmanni of Myyrmäki Cleaning, Shelving, Customer Service</p>
            <p>3/15/2020 – 3/27/2020&#128330; Trainee Martinlaakson’s  S_Market Shelving, Customer Service</p>
-           <p>I have worked at Eezy and Varapu a few times in grocery stores.</p>
+           <p>I have worked at Eezy Oy, and Vara_apu Oy, A few times in grocery stores.</p>
            <p>In working life, I have done teamwork and sometimes I was alone.</p>
            <p>1/10/2020 - now&#128330; Job Foodora/Wolt food delivery</p>
          </h1>
